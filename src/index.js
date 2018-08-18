66c26d4024708f0ff71906223d7749da305c6c1f
leo <leoliangliwen@gmail.com>
2018-08-18 11:44:40 -0400

    reorg dir

--- src/index.js
+++ src/index.js
@@ -1,39 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
-
-function timeout(duration = 0) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => resolve(duration), duration);
-    })
-}
-
-/*
-// Then
-timeout(3000).then( (v) => {
-        console.log('resolved with: ' + v);
-        return timeout(3001);
-    }, (v) => {
-        console.log('rejected with: ' + v);
-    }).then( (v) => {
-        console.log('resolved with1: ' + v);
-    }, (v) => {
-        console.log('rejected with1: ' + v);
-    })
-*/
-// Catch
-timeout(1000).then((v) => {
-    console.log('resolved with: ' + v);
-    throw Error("hmm");
-}, (v) => {
-    console.log('rejected with: ' + v);
-}).catch((err) => {
-    console.log(err);
-})
-
-
