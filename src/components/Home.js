8a480694343f35dc62584dee3d78169b87249d92
leo <leoliangliwen@gmail.com>
2018-08-19 12:01:56 -0400

    add geolocation

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import { Tabs, Button } from 'antd';
+import {GEO_OPTIONS} from '../constants'
 
 export class Home extends React.Component {
+
+    componentDidMount(){
+        this.getGeolocation();
+    }
+
+    getGeolocation = () => {
+        if ("geolocation" in navigator) {
+            navigator.geolocation.getCurrentPosition(
+                this.onSuccessLoadGeoLocation,
+                this.onFailedLoadGeolocation,
+                GEO_OPTIONS,
+            );
+        } else {
+
+        }
+    }
+
+    onSuccessLoadGeoLocation = (position) => {
+        console.log(position);
+    }
+    onFailedLoadGeolocation = () => {
+        console.log("Fail to load geo location.");
+    }
+
     render() {
+        const TabPane = Tabs.TabPane;
+        const operations = <Button type="primary">Create New Post</Button>;
         return (
-            <div>this is home</div>
+            <Tabs tabBarExtraContent={operations} className = "main-tabs">
+                <TabPane tab="Image Post" key="1">Image Posts</TabPane>
+                <TabPane tab="Video Posts" key="2">Video Posts</TabPane>
+                <TabPane tab="Map" key="3">Map</TabPane>
+            </Tabs>
         );
     }
 }
--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,8 @@
 export const API_ROOT = 'https://around-75015.appspot.com/api/v1';
 export const TOKEN_KEY = 'TOKEN_KEY';
+export const GEO_OPTIONS = {
+    enableHighAccuracy: true,  
+    maximumAge        : 360000,  
+    timeout           : 27000 
+};
 
